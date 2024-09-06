25c9b70b0f9b7702c5c48dead493fc81bee64f60
tsikot39 <corpusjohnbenedict@gmail.com>
2024-09-05 18:34:07 -0600

    first commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'cc7df445-24ee-447c-915b-3dad6c1d99e2';
+export const MODAL_CLOSE_SEC = 2.5;
